7f9e62de6b50c9688d7339494d4ae1f2c0ac8f24
rtwnt <contact@reusingthewheel.net>
2022-04-02 20:49:30 +0200

    new: add lambda-fairy/maud crate
    This commit also implements a small example from documentation: https://maud.lambda.xyz/

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use maud::html;
+
 fn main() {
-    println!("Hello, world!");
+    let content = html! {
+        h1 { "Hello, world!" }
+        p.intro {
+            "This is an example of the "
+            a href="https://github.com/lambda-fairy/maud" { "Maud" }
+            " template language."
+        }
+    };
+
+    println!("{}", content.into_string());
 }
